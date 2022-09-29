407a629b6eb4b4540f7579932359f0ab7340f3a2
Nguyen Huy Hieu <hieunhgch18038@gmail.com>
2022-09-29 13:57:08 +0700

    dsa

--- lab6/app.js
+++ lab6/app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const { render } = require("pug");
 const port = process.env.PORT || 8000;
 
 //set thu muc chua views
@@ -11,7 +12,16 @@ app.set("view engine", "hbs");
 
 //render ra trang home (index.hbs)
 //get: get method
-app.get("/", (req, res) => {app.render("index");});
+app.get("/", (req, res) => {
+    let name = "Greenwich university";
+    let address = "2 pham van bach";
+    res.render("index", { n: name, a: address});
+});
+app.get("/city", (req, res) => {
+    //let country = "VietNam";
+    let cities = ["Hanoi", "HCM", "Da nang", "Can tho"];
+    res.render("city", { country: "Vietnam", cities: cities});
+});
 
 //chay web server 
 app.listen(port, () => {
